5fdaa28e559e8aa253dfc6fe4e1bd39e92f65bc5
Natalia Alfaro <natalia.alfaro@truextend.com>
2016-04-15 14:48:21 -0400

    fixing authorizer

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,5 +3,5 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.RESTAdapter.extend(DataAdapterMixin,{
 	host:'http://192.168.1.172/KidsChooseApi/v1.0/mobile',
-	authorizer: "authorizer:custom"
+	authorizer: 'authorizer:custom'
 });
--- app/authorizers/custom.js
+++ app/authorizers/custom.js
@@ -1,11 +1,17 @@
 import Ember from 'ember';
 import Base from 'ember-simple-auth/authorizers/base';
 export default Base.extend({
-    authorize: function(jqXHR, requestOptions) {
-
-    	var accessToken = this.get('session.session.content.authenticator.token');
-        if (this.get('session.isAuthenticated') && !Ember.isEmpty(accessToken)) {
-            jqXHR.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-        }
+	authorize(sessionData, block) {
+    if (!Ember.isEmpty(sessionData.token)) {
+      block('Authorization',  'Bearer '+sessionData.token);
+      block('Content-Type',  'application/json');
     }
+  }
+//    authorize: function(jqXHR, requestOptions) {
+
+//    var accessToken = this.get('session.session.content.authenticator.token');
+//        if (this.get('session.isAuthenticated') && !Ember.isEmpty(accessToken)) {
+//            jqXHR.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+//        }
+//    }
 });
