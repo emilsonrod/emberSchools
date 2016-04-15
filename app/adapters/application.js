7bc0ee15acd25ea2572b2d3e0bc4e4367ac57ecc
Natalia Alfaro <natalia.alfaro@truextend.com>
2016-04-15 11:13:13 -0400

    editing adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.RESTAdapter.extend({
+export default DS.RESTAdapter.extend(DataAdapterMixin,{
 	host:'http://192.168.1.172/KidsChooseApi/v1.0/mobile',
 	authorizer: "authorizer:custom"
 });
